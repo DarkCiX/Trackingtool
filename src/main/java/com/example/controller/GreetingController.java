bde4d761ddd4a2d21a0e421a8f09d540decfea27
DarkCiX <julychantony@gmail.com>
2022-11-30 20:52:41 +0300

    update

--- src/main/java/com/example/controller/GreetingController.java
+++ src/main/java/com/example/controller/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.model.HelloMessage;
 
